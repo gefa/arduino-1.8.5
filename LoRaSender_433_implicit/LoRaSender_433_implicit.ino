60337e4bf6caa1b2307cade2ae45be5a91bf2b38
stefan gvozdenovic <tesla@bu.edu>
2021-01-27 12:54:59 -0500

    433MHz loRa Tx, works with https://www.epfl.ch/labs/tcl/resources-and-sw/lora-phy/ grc3.7 receiver

--- LoRaSender_433_implicit/LoRaSender_433_implicit.ino
+++ LoRaSender_433_implicit/LoRaSender_433_implicit.ino
@@ -8,6 +8,8 @@ void setup() {
   while (!Serial);
 
   Serial.println("LoRa Sender");
+  // ss,reset,dio 4,2,3
+  //LoRa.setPins(8,4,7);
   LoRa.setPins(4,2,3);
   
   if (!LoRa.begin(433E6)) {
@@ -22,12 +24,12 @@ void loop() {
   Serial.println(counter);
 
   // send packet
-  LoRa.beginPacket(1); // 1 for implicit header mode
+  LoRa.beginPacket(0); // 1 for implicit header mode
   LoRa.print("hello ");
   LoRa.print(counter);
   LoRa.endPacket();
 
   counter++;
 
-  delay(2000);
+  delay(1000);
 }
